40f05555ffe1edf717dd975348c149ea071f73ce
Hemanth Mandali <mandalihemanthkrishna@gmail.com>
2020-12-16 21:24:54 +0530

    Int: Added code for Mapping components, i.e instead of single components being repetitive , we can use map and reduce

--- ReactPractice/my-app/src/Components/JokeMain.js
+++ ReactPractice/my-app/src/Components/JokeMain.js
@@ -1,36 +1,17 @@
 import React from "react"
 
 import Joke from "./Joke"
+import jokesData from "./jokesData"
 function JokeMain(){
+    // we can map the components of jokesdata (array of jokes ), instead of passing each argument
+
+    const jokeComponent = jokesData.map(function(joke) {
+        return <Joke question = {joke.question} punchLine ={joke.punchLine} />
+    })
+
     return(
         <div>
-            <Joke
-                punchLine = "Theres no question" 
-            />
-            <Joke 
-                question="What's the best thing about Switzerland?" 
-                punchLine="I don't know, but the flag is a big plus!"
-            />
-            
-            <Joke 
-                question="Did you hear about the mathematician who's afraid of negative numbers?" 
-                punchLine="He'll stop at nothing to avoid them!"
-            />
-            
-            <Joke 
-                question="Hear about the new restaurant called Karma?" 
-                punchLine="There’s no menu: You get what you deserve."
-            />
-            
-            <Joke 
-                question="Did you hear about the actor who fell through the floorboards?" 
-                punchLine="He was just going through a stage."
-            />
-            
-            <Joke 
-                question="Did you hear about the claustrophobic astronaut?" 
-                punchLine="He just needed a little space."
-            />
+            {jokeComponent}
         </div>
     )
 }
