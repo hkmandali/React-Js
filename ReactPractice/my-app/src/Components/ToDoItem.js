8b57e2d227e5a692f1b3759dd1136fc3da67dd75
Hemanth Mandali <mandalihemanthkrishna@gmail.com>
2020-12-17 13:08:42 +0530

    Int: Updated states from parent to child and vice versa

--- ReactPractice/my-app/src/Components/ToDoItem.js
+++ ReactPractice/my-app/src/Components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function ToDoItem(props){
     return(
         <div className="todo-item">
-            <input type ="checkbox" checked={props.item.completed}/>
+            <input type ="checkbox" onChange ={() => props.handleChange(props.item.id)}/>
             <p>{props.item.text}</p>
         </div>
     )
