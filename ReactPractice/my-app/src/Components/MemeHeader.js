73223929a6eef0575f3773831a4279fc18bf1734
Hemanth Mandali <mandalihemanthkrishna@gmail.com>
2020-12-18 13:01:57 +0530

    Added code for memgenerator , this can be directly used to generate memes based on random meme images and our custom text

---
+++ ReactPractice/my-app/src/Components/MemeHeader.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+class MemeHeader extends React.Component{
+    render(){
+        return (
+        <header> <img 
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+        alt="Problem?"
+        />
+
+    </header>
+        )
+    }
+}
+
+export default MemeHeader
